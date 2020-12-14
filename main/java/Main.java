880bc824f71e21ff4a3f9d21fcec3a358d6fef06
Razercake <45364954+Razercake@users.noreply.github.com>
2020-12-14 10:29:20 +0100

    Order branch

--- main/java/Main.java
+++ main/java/Main.java
@@ -3,8 +3,11 @@ import java.sql.*;
 
 public class Main {
 
+    // det her er vores order branch
+
     public static void main(String[] args) {
         MainMenu mainMenu = new MainMenu();
         mainMenu.mainMenuLoop();
     }
+
 }
