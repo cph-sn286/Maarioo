7a1af987bdd5a834bb54c355743412e3b21634e9
Razercake <45364954+Razercake@users.noreply.github.com>
2021-01-04 11:14:47 +0100

    merget statistik med main

--- main/java/Main.java
+++ main/java/Main.java
@@ -6,7 +6,7 @@ import java.sql.*;
 
 public class Main {
 
-    // her laver vi sortering af ordre liste
+    // Main merge
 
     public static void main(String[] args) throws MarioException, IOException {
         MainMenu mainMenu = new MainMenu();
